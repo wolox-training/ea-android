b098427e5c8ee304e08ba81e73f225115b5aaaa5
ErikEmmanuelAnrique <erik.anrique@wolox.com.ar>
2021-04-06 14:45:41 -0300

    Implemented deltaTime function.

--- app/src/main/java/ar/com/wolox/android/features/homepage/news/NewsViewHolder.kt
+++ app/src/main/java/ar/com/wolox/android/features/homepage/news/NewsViewHolder.kt
@@ -1,5 +1,6 @@
 package ar.com.wolox.android.features.homepage.news
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.view.View
 import android.widget.ImageView
@@ -8,6 +9,8 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import ar.com.wolox.android.R
 import ar.com.wolox.android.models.News
+import java.time.Instant
+import java.time.temporal.ChronoUnit
 
 class NewsViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
@@ -17,13 +20,27 @@ class NewsViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     val viewTime: TextView = view.findViewById(R.id.timeTextView)
     val viewImage: ImageView = view.findViewById(R.id.newsImageView)
 
+    @SuppressLint("NewApi")
     fun populate(dataSet: News?, context: Context) {
         with(dataSet!!) {
             viewTitle.text = commenter
             viewDescription.text = comment
-            viewTime.text = date
+            viewTime.text = deltaTime(updated_at)
             // Glide.with(context).clear(viewImage)
             // Glide.with(context).load(avatar).into(viewImage)
+        }
+    }
+
+    @SuppressLint("NewApi")
+    private fun deltaTime(lastUpdated: String): String {
+
+        val currentTime = Instant.now()
+        val lastUpdatedTime = Instant.parse(lastUpdated)
+
+        return when (val period = ChronoUnit.MINUTES.between(lastUpdatedTime, currentTime)) {
+            in 0..60 -> "${period}m"
+            in 60..1440 -> "${period / 60}h"
+            else -> "${period / 1440}d"
         }
     }
 }
